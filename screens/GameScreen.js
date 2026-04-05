87d87475b772b308383e02d289eec42148e1edd2
ambuj vashistha <ambujva123@gmail.com>
2026-04-05 15:12:28 +0530

    Add SafeAreaView to GameScreen for proper layout spacing

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,9 @@
 import { View, StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { useState } from "react";
 import Grid from "../components/Grid";
 import Keyboard from "../components/Keyboard";
+import { WORDS_4, WORDS_5, WORDS_6 } from "../utils/Words";
 
 export default function GameScreen({ route }) {
   const { wordLength } = route.params;
@@ -12,6 +14,18 @@ export default function GameScreen({ route }) {
   );
   const [currentRow, setCurrentRow] = useState(0);
   const [currentCol, setCurrentCol] = useState(0);
+  
+  const getRandomWord = () => {
+    let list;
+
+    if (wordLength === 4) list = WORDS_4;
+    if (wordLength === 5) list = WORDS_5;
+    if (wordLength === 6) list = WORDS_6;
+
+    return list[Math.floor(Math.random() * list.length)];
+  };
+
+  const [targetWord, setTargetWord] = useState(getRandomWord());
 
   const handleKeyPress = (key) => {
     if (currentCol >= wordLength) return;
@@ -36,31 +50,52 @@ export default function GameScreen({ route }) {
   const handleEnter = () => {
     if (currentCol < wordLength) return;
 
+    const currentGuess = guesses[currentRow].join("");
+
+    console.log("Guess:", currentGuess);
+    console.log("Target:", targetWord);
+    if (currentGuess === targetWord) {
+      console.log("WIN 🎉");
+    }
+
     setCurrentRow((prev) => prev + 1);
     setCurrentCol(0);
   };
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text style={styles.heading}>Wordle</Text>
-      <Grid guesses={guesses} currentRow={currentRow} currentCol={currentCol} />
+
+      <View style={styles.gridContainer}>
+        <Grid guesses={guesses} />
+      </View>
+
       <Keyboard
         onKeyPress={handleKeyPress}
         onDelete={handleDelete}
         onEnter={handleEnter}
       />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 60,
+    backgroundColor: "#fff",
   },
-  heading:{
-    fontSize:50,
-    fontWeight:"700",
-  }
+
+  heading: {
+    fontSize: 40,
+    fontWeight: "700",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+
+  gridContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
---
+++ utils/Words.js
@@ -0,0 +1,3 @@
+export const WORDS_4 = ["GAME", "PLAY", "CODE", "WORD"];
+export const WORDS_5 = ["APPLE", "TRAIN", "BRICK", "PLANT"];
+export const WORDS_6 = ["POCKET", "STREAM", "OBJECT", "MARKET"];
